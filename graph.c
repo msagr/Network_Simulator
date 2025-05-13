7c20c69f34004e9cedb35e430ac40fadadd5b551
msagr <mehulsagrawal33@gmail.com>
2025-05-13 17:19:10 +0530

    implemented routines to enable communication between nodes

--- graph.c
+++ graph.c
@@ -17,6 +17,9 @@ node_t* create_graph_node(graph_t *graph, char *node_name) {
   strncpy(node->node_name, node_name, NODE_NAME_SIZE);
   node->node_name[NODE_NAME_SIZE] = '\0';
 
+  init_udp_socket(node);
+
+  init_node_nw_prop(&node->node_nw_prop);
   init_glthread(&node->graph_glue);
   glthread_add_next(&graph->node_list, &node->graph_glue);
   return node;
