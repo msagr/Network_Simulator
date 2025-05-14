d51cf055ab6f446b00a378f0846f407d612cbfc1
msagr <mehulsagrawal33@gmail.com>
2025-05-14 17:31:45 +0530

    added ARP table lookup api

--- Layer2/layer2.c
+++ Layer2/layer2.c
@@ -6,3 +6,18 @@ init_arp_table(arp_table_t **arp_table){
     *arp_table = calloc(1, sizeof(arp_table_t));
     init_glthread(&((*arp_table)->arp_entries));
 }
+
+arp_entry_t *
+arp_table_lookup(arp_table_t *arp_table, char *ip_addr){
+
+    glthread_t *curr;
+    arp_entry_t *arp_entry;
+    ITERATE_GLTHREAD_BEGIN(&arp_table->arp_entries, curr){
+    
+        arp_entry = arp_glue_to_arp_entry(curr);
+        if(strncmp(arp_entry->ip_addr.ip_addr, ip_addr, 16) == 0){
+            return arp_entry;
+        }
+    } ITERATE_GLTHREAD_END(&arp_table->arp_entries, curr);
+    return NULL;
+}
